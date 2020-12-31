59b58064d9cca8628e2f50ebb63269776b0fb574
czhizhpon <czhizhpon@est.ups.edu.ec>
2020-12-31 18:22:42 -0500

    SIA-8 #comment [MAT-1] Implementación del controlador para inscripción #in-progress

---
+++ src/ec/edu/ups/controller/registration/InscriptionController.java
@@ -0,0 +1,72 @@
+package ec.edu.ups.controller.registration;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ec.edu.ups.dao.DAOFactory;
+import ec.edu.ups.dao.registration.InscriptionDAO;
+import ec.edu.ups.entities.registration.Inscription;
+
+/**
+ * Servlet implementation class InscriptionController
+ */
+@WebServlet("/InscriptionController")
+public class InscriptionController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private InscriptionDAO inscriptionDAO;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InscriptionController() {
+        super();
+        inscriptionDAO = DAOFactory.getFactory().getInscriptionDAO();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String option;
+		String output = "";
+		try {
+			option = request.getParameter("option");
+			switch (option) {
+			case "create":
+				output = createInscription(request);
+				break;
+			default:
+				break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		request.setAttribute("output", output);
+	}
+	
+	private String createInscription(HttpServletRequest request) {
+		int studentId;
+		int careerId;
+		Inscription inscription;
+		
+		try {
+			studentId = Integer.parseInt(request.getParameter("stu_id"));
+			careerId = Integer.parseInt(request.getParameter("car_id"));
+			inscription = new Inscription();
+			inscription.setDate(new Date());
+			inscriptionDAO.create(inscription);
+			return "Success";
+		} catch (Exception e) {
+			return "Error";
+		}
+	}
+	
+}
---
+++ src/ec/edu/ups/test/controller/registration/InscriptionControllerTest.java
@@ -0,0 +1,54 @@
+package ec.edu.ups.test.controller.registration;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ec.edu.ups.controller.registration.InscriptionController;
+
+class InscriptionControllerTest {
+
+	private InscriptionController inscriptionController;
+	private HttpServletRequest request;
+    private HttpServletResponse response;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		inscriptionController = new InscriptionController();
+		request = mock(HttpServletRequest.class);       
+        response = mock(HttpServletResponse.class); 
+	}
+
+	@Test
+	void test() throws ServletException, IOException {
+		String output;
+		
+		when(request.getParameter("option")).thenReturn("create");
+        when(request.getParameter("stu_id")).thenReturn("1");
+        when(request.getParameter("car_id")).thenReturn("1");
+		
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
+        
+        inscriptionController.doGet(request, response);
+        
+        verify(request, atLeast(1)).getParameter("option");
+        writer.flush();
+        
+		assertEquals("Success", stringWriter.toString());
+		
+		System.out.println("Response: " + stringWriter.toString());
+	}
+
+}
