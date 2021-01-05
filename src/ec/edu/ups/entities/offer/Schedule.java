4ec200706b7e5d80d92fc74b717a0fa44c3e545f
robyserpa <51723735+robyserpa@users.noreply.github.com>
2021-01-05 18:04:25 -0500

    SIA-19 #in-progress  #comment [OFE-6] implementación de get y set

--- src/ec/edu/ups/entities/offer/Schedule.java
+++ src/ec/edu/ups/entities/offer/Schedule.java
@@ -34,5 +34,59 @@ public class Schedule implements Serializable {
 	public Schedule() {
 		super();
 	}
+
+	public Schedule(String day, String startTime, String endTime, Group group) {
+		super();
+		this.day = day;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.group = group;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+
+	public Group getGroup() {
+		return group;
+	}
+
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+
+	@Override
+	public String toString() {
+		return "Schedule [id=" + id + ", day=" + day + ", startTime=" + startTime + ", endTime=" + endTime + ", group="
+				+ group + "]";
+	}
    
 }
