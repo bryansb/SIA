8953d4e98a7ae00fcea3156446d197698da41722
czhizhpon <czhizhpon@est.ups.edu.ec>
2021-01-07 19:55:21 -0500

    SIA-24 #comment Pruebas unitarias para facturación #in-progress

--- src/ec/edu/ups/test/entities/accounting/BillDetailTest.java
+++ src/ec/edu/ups/test/entities/accounting/BillDetailTest.java
@@ -5,25 +5,46 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import ec.edu.ups.dao.DAOFactory;
+import ec.edu.ups.dao.accounting.BillDetailDAO;
+import ec.edu.ups.dao.accounting.BillHeadDAO;
 import ec.edu.ups.entities.accounting.BillDetail;
+import ec.edu.ups.entities.accounting.BillHead;
 
 class BillDetailTest {
 
 	private BillDetail billDetail;
+	private BillHead billHead;
+	private BillDetailDAO billDetailDAO;
+	private BillHeadDAO billHeadDAO;
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		billDetail = new BillDetail();
-		billDetail.setQuantity(5);
+		this.billDetail = new BillDetail();
+		this.billHead = new BillHead();
+		this.billHeadDAO = DAOFactory.getFactory().getBillHeadDAO();
+		this.billDetailDAO = DAOFactory.getFactory().getBillDetailDAO();
+		this.billDetail.setQuantity(5);
 	}
 
 	@Test
-	void test() {
-		billDetail.setUnitPrice(2.5);
-		billDetail.calculateTotal();
+	void test() throws Exception {
+		BillDetail billDetail;
+		createBillHead();
+		this.billDetail.setUnitPrice(2.5);
+		this.billDetail.calculateTotal();
+		this.billDetail.setBillHead(this.billHead);
+		this.billDetailDAO.create(this.billDetail);
+		billDetail = billDetailDAO.read(1);
 		double expectedTotal = 12.5;
 		double calculatedTotal = billDetail.getTotal();
 		assertEquals(expectedTotal, calculatedTotal);
+	}
+	
+	void createBillHead() throws Exception {
+		this.billHead = new BillHead();
+		this.billHeadDAO.create(billHead);
+		this.billHead = this.billHeadDAO.read(1);
 	}
 
 }
--- src/ec/edu/ups/test/entities/accounting/BillHeadTest.java
+++ src/ec/edu/ups/test/entities/accounting/BillHeadTest.java
@@ -6,28 +6,35 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ec.edu.ups.controller.utils.ParameterBasicCreation;
+import ec.edu.ups.dao.DAOFactory;
+import ec.edu.ups.dao.accounting.BillHeadDAO;
 import ec.edu.ups.entities.accounting.BillHead;
 
 class BillHeadTest {
 
 	private BillHead billHead;
 	private ParameterBasicCreation parameterBasicCreation;
+	private BillHeadDAO billHeadDAO;
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		billHead = new BillHead();
-		parameterBasicCreation = new ParameterBasicCreation();
-		parameterBasicCreation.init();
-		billHead.createBillDetail("", 10, 1.0);
-		billHead.createBillDetail("", 10, 2.0);
-		billHead.createBillDetail("", 10, 3.0);
+		this.billHead = new BillHead();
+		this.parameterBasicCreation = new ParameterBasicCreation();
+		this.billHeadDAO = DAOFactory.getFactory().getBillHeadDAO();
+		this.parameterBasicCreation.init();
+		this.billHead.createBillDetail("", 10, 1.0);
+		this.billHead.createBillDetail("", 10, 2.0);
+		this.billHead.createBillDetail("", 10, 3.0);
 	}
 
 	@Test
-	void test() {
-		billHead.calculateTotal();
+	void test() throws Exception {
+		double calculatedTotal;
 		double expectedTotal = 70.2;
-		double calculatedTotal = billHead.getTotal();
+		this.billHead.calculateTotal();
+		billHeadDAO.create(this.billHead);
+		calculatedTotal = this.billHead.getTotal();
+		System.out.println(" >>>>>>> " + calculatedTotal);
 		assertEquals(expectedTotal, calculatedTotal);
 	}
 
