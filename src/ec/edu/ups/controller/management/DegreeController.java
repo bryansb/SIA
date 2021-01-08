c4e9761e13d9a1e8ca4227e2187c2e622cb03499
dani-alv97 <charlialvarezbrau@hotmail.com>
2021-01-08 02:40:57 -0500

    SIA-5 #comment [GES-2] Creacion del controlador de Degree #in-progress

---
+++ src/ec/edu/ups/controller/management/DegreeController.java
@@ -0,0 +1,131 @@
+package ec.edu.ups.controller.management;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ec.edu.ups.dao.DAOFactory;
+import ec.edu.ups.dao.management.DegreeDAO;
+import ec.edu.ups.dao.management.TeacherDAO;
+import ec.edu.ups.entities.management.Degree;
+import ec.edu.ups.entities.management.Teacher;
+
+@WebServlet("/DegreeController")
+public class DegreeController extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	private DegreeDAO degreeDAO;
+	private TeacherDAO teacherDAO;
+	
+	public DegreeController() {
+		super();
+		degreeDAO = DAOFactory.getFactory().getDegreeDAO();
+		teacherDAO = DAOFactory.getFactory().getTeacherDAO();
+	}
+	
+	private String createDegree(HttpServletRequest request) {
+		Degree degree;
+		int teacherId;
+		List<Teacher> teacherList;
+		try {
+			teacherId = Integer.parseInt(request.getParameter("use_id"));
+			teacherList = teacherDAO.find("", teacherId, 0);
+			degree = new Degree();
+			degree.setName(request.getParameter("deg_name"));
+			degree.setTeacherList(teacherList);
+			degreeDAO.create(degree);
+			return "Success";
+		}catch(Exception e) {
+			return "Error";
+		}
+	}
+	
+	private String updateDegree(HttpServletRequest request) {
+		int degreeId;
+		Degree degree;
+		int teacherId;
+		List<Teacher> teacherList;
+		try {
+			degreeId = Integer.parseInt(request.getParameter("deg_id"));
+			degree = degreeDAO.read(degreeId);
+			teacherId = Integer.parseInt(request.getParameter("use_id"));
+			teacherList = teacherDAO.find("", teacherId, 0);
+			degree.setName(request.getParameter("deg_name"));
+			degree.setTeacherList(teacherList);
+			degreeDAO.update(degree);
+			return "Success";
+		}catch(Exception e) {
+			return "Error";
+		}
+	}
+	
+	private Degree readDegree(HttpServletRequest request) {
+		int degreeId;
+		Degree degree;
+		try {
+			degreeId = Integer.parseInt(request.getParameter("deg_id"));
+			degree = degreeDAO.read(degreeId);
+			return degree;
+		}catch(Exception e) {
+			return null;
+		}
+	}
+	
+	private String deleteDegree(HttpServletRequest request) {
+		int degreeId;
+		try {
+			degreeId = Integer.parseInt(request.getParameter("deg_id"));
+			degreeDAO.deleteByID(degreeId);
+			return "Success";
+		}catch(Exception e) {
+			return "Error";
+		}
+	}
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String option;
+		String output = "";
+		try {
+			option = request.getParameter("option");
+			switch (option) {
+			case "create":
+				output = createDegree(request);
+				break;
+			case "update":
+				output = updateDegree(request);
+				break;
+			case "read":
+				request.setAttribute("degree", readDegree(request));
+				break;
+			case "delete":
+				output = deleteDegree(request);
+				break;
+			default:
+				break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		request.setAttribute("output", output);
+		response.getWriter().append(output);
+	}
+
+	public void doTest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Degree degree;
+		this.doGet(request, response);
+		degree = readDegree(request);
+		if(degree == null) {
+			response.getWriter().append("Error");
+		}else {
+			response.getWriter().append("Success");
+		}
+	}
+}
