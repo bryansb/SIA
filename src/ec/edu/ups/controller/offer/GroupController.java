b6a9b154f0832cf48642cbc1ddb430becf7e616b
robyserpa <rserpa@est.ups.edu.ec>
2021-02-05 03:07:55 -0500

    SIA-28 #comment [OFE-9] Registro #testing

--- src/ec/edu/ups/controller/offer/GroupController.java
+++ src/ec/edu/ups/controller/offer/GroupController.java
@@ -15,12 +15,10 @@ import javax.servlet.http.HttpServletResponse;
 import ec.edu.ups.dao.DAOFactory;
 import ec.edu.ups.dao.management.TeacherDAO;
 import ec.edu.ups.dao.offer.GroupDAO;
-import ec.edu.ups.dao.offer.ScheduleDAO;
 import ec.edu.ups.dao.offer.SubjectDAO;
 import ec.edu.ups.dao.utils.ParameterDAO;
 import ec.edu.ups.entities.management.Teacher;
 import ec.edu.ups.entities.offer.Group;
-import ec.edu.ups.entities.offer.Schedule;
 import ec.edu.ups.entities.offer.Subject;
 
 /**
